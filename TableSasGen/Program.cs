e03770915e6de1627761b7b35180c8d23e385907
Suwath Ch <suwatch@microsoft.com>
2015-10-08 14:59:00 -0700

    initial commit

---
+++ TableSasGen/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableSasGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PolicyName = "TableSasGenPolicy";
+
+            try
+            {
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Usage: TableSasGen.exe <storageAccount> <tableName> <storageKey>");
+                    return;
+                }
+
+                var storageAccount = args[0];
+                var tableName = args[1];
+                var storageKey = args[2];
+
+                var storageUri = new Uri(String.Format("https://{0}.table.core.windows.net/", storageAccount));
+
+                CloudTableClient client = new CloudTableClient(storageUri, new StorageCredentials(storageAccount, storageKey));
+                CloudTable table = client.GetTableReference(tableName);
+                TablePermissions permissions = table.GetPermissions();
+                SharedAccessTablePolicy policy = null;
+                if (!permissions.SharedAccessPolicies.ContainsKey(PolicyName))
+                {
+                    policy = new SharedAccessTablePolicy()
+                    {
+                        Permissions = SharedAccessTablePermissions.Add | SharedAccessTablePermissions.Delete | SharedAccessTablePermissions.Query | SharedAccessTablePermissions.Update,
+                        SharedAccessExpiryTime = DateTimeOffset.MaxValue,
+                    };
+
+                    permissions.SharedAccessPolicies.Add(PolicyName, policy);
+
+                    table.SetPermissions(permissions);
+
+                }
+                else
+                {
+                    policy = permissions.SharedAccessPolicies[PolicyName];
+                }
+
+                var sasToken = table.GetSharedAccessSignature(policy);
+                Console.WriteLine("{0}{1}", storageUri, sasToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
